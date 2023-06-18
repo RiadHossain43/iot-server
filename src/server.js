15c029300f0dba42f0a5bb0a2f0f862ed302b787
RiadHossain43 <riadhossain7464@gmail.com>
2023-06-18 12:43:33 +0600

    created

--- src/server.js
+++ src/server.js
@@ -28,6 +28,16 @@ app.use(compression());
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ limit: "200mb", extended: true }));
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+app.use((req, res, next) => {
   console.log(new Date(), "↓");
   console.info(
     "request: ",
